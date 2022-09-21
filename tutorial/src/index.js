c1611c935bab8d759f1177ba1ac93f0ecf35b18d
Bunny Schaefer <SchaeferBunny@gmail.com>
2022-09-21 11:02:57 -0700

    Comments created

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,7 +1,8 @@
 import ReactDom from 'react-dom'
 import Testing from './testing'
 
-
+//Stateless functional component
+//always return JSX
 
 function Greeting(){
   return <div>This is Bunny and this is my first component.
