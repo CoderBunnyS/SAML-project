2d238f9e3ab40c0757eb050a479f8920bfecf34a
Bunny Schaefer <SchaeferBunny@gmail.com>
2022-11-02 09:23:44 -0700

    refactoring

--- tutorial/src/book.js
+++ tutorial/src/book.js
@@ -1,16 +1,16 @@
 import ReactDom from 'react-dom'
 
-
-
-const Book = () => {
     const title = 'Harry Potter and the Order of the Phoenix'
     const author = 'JK Rowling'
     const image = "https://images-na.ssl-images-amazon.com/images/I/71tR2ZEgPYL._AC_UL254_SR254,254_.jpg"
-  
+
+const Book = (props) => {
+    console.log(props)
   return <article className='book'>
-    <img src={image} alt="Book Cover"></img>
-    <h1>{title}</h1>
-    <h4>{author}</h4>
+    <img src={props.image} alt="Book Cover"></img>
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
+    <p>{props.title}</p>
 </article>
 }
 
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,15 +1,17 @@
 import ReactDom from 'react-dom'
 import Book from './book'
 import './index.css'
+
 //import Testing from './testing'
 
 //Stateless functional component
 //We always return JSX
 //Nested Components, React Tools
 
-function BookList(){
+function BookList(props){
   return <section className='booklist'>
-    <Book />
+    <Book job="developer" />
+    <Book title="random title" number={22}/>
   </section>
 
 }
